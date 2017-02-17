5a2f13c57669c8c92f5ab6bd93cfb5de59bd0554
redpage <redpage.park@gmail.com>
2017-02-17 12:19:20 +0900

    v1.0.0.0

--- app/main.js
+++ app/main.js
@@ -13,17 +13,28 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    "web-preferences" : {
+      "web-security" : false
+    },
+    frame : false,
+    "width" : 980,
+    "height" : 650,
+    "minWidth": 980, 
+    "minHeight" : 650
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
+
+  // mainWindow.loadURL("http://smgr.winnerit.co.kr/www/theme.winnerstock.co.kr");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -31,7 +42,9 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-  })
+  });
+
+  //mainWindow.maximize();
 }
 
 // This method will be called when Electron has finished
